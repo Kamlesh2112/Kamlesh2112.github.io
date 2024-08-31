21bc3cb47008b8609bba1fe788c37df077db9c6c
kamlesh2112sawale <kamlesh2112sawale@gmail.com>
2024-08-31 16:32:15 +0000

    directory name changed to kamlesh

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '4 chan',
+      title: 'Kamlesh Savale',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
