bada9f5d4eeab93a77b23bb7b4ca799eab8dbdb2
kamlesh2112sawale <kamlesh2112sawale@gmail.com>
2024-08-31 14:49:37 +0000

    Initial commit for gh-pages branch

---
+++ lib/constants/global_variable.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter/material.dart';
+
+class GlobalVariables{
+  // 4chan color pallet
+  static const brown = 0xFF5E3C2A;
+  static const red = 0xFFB63F3B;
+  static const greentext = 0xFFC8F522;
+  static const postbody = 0xFFB8A4B5;
+  static const postheader = 0xFF886E99;
+}
---
+++ lib/widgets/post.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/constants/global_variable.dart';
+
+class Post extends StatelessWidget {
+  const Post({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String now = DateTime.now().toString();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: const Color(GlobalVariables.postbody),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              color: const Color(GlobalVariables.postheader),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.more_vert),
+                  const Text(
+                    'Anonymous',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color:Color.fromARGB(255, 0, 113, 4),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                  ),
+                  Text(
+                    now,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              alignment: Alignment.topLeft,
+              child: const Text(
+                '> be me \n > flutter developer',
+                style: TextStyle(
+                  color: Color(GlobalVariables.greentext),
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
